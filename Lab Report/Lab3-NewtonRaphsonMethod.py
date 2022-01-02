38aa9e153a51acb529269ea02399ab4b0cd108b9
kingdragon100 <kabirdeula167@gmail.com>
2022-01-02 19:09:54 +0545

    updated Newton Raphson

--- Lab Report/Lab3-NewtonRaphsonMethod.py
+++ Lab Report/Lab3-NewtonRaphsonMethod.py
@@ -7,7 +7,6 @@ def g(x):
     return 3*x**2 - 5
 
 # Implementing Newton Raphson Method
-
 def newtonRaphson(x0,e,N):
     print('\n\n*** NEWTON RAPHSON METHOD IMPLEMENTATION ***')
     step = 1
@@ -33,20 +32,6 @@ def newtonRaphson(x0,e,N):
         print('\nRequired root is: %0.8f' % x1)
     else:
         print('\nNot Convergent.')
-
-
-# Input Section
-# x0 = input('Enter Guess: ')
-# e = input('Tolerable Error: ')
-# N = input('Maximum Step: ')
-
-# Converting x0 and e to float
-# x0 = float(x0)
-# e = float(e)
-
-# Converting N to integer
-# N = int(N)
-
 
 #Note: You can combine above three section like this
 x0 = float(input('Enter Guess: '))
