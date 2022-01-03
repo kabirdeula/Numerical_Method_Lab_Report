c661dac2ecd479a278e469a9f5c4583388e88934
kingdragon100 <kabirdeula167@gmail.com>
2022-01-03 12:39:32 +0545

    updated Error

--- Lab Report/Lab3-NewtonRaphsonMethod.py
+++ Lab Report/Lab3-NewtonRaphsonMethod.py
@@ -7,24 +7,23 @@ def g(x):
     return 3*x**2 - 5
 
 # Implementing Newton Raphson Method
-def newtonRaphson(x0,e,N):
+def newtonRaphson(x0,e):
     print('\n\n*** NEWTON RAPHSON METHOD IMPLEMENTATION ***')
     step = 1
     flag = 1
     condition = True
+    print('Iteration\t\tx1\t\tf(x1)\t\tError')
     while condition:
         if g(x0) == 0.0:
             print('Divide by zero error!')
             break
         
         x1 = x0 - f(x0)/g(x0)
-        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, x1, f(x1)))
+        err = (x1 - x0)/x1
+        print('%d\t\t%0.6f\t%0.6f\t\t%0.6f'%(step, x1, f(x1), err))
+        
         x0 = x1
         step = step + 1
-        
-        if step > N:
-            flag = 0
-            break
         
         condition = abs(f(x1)) > e
     
@@ -36,7 +35,6 @@ def newtonRaphson(x0,e,N):
 #Note: You can combine above three section like this
 x0 = float(input('Enter Guess: '))
 e = float(input('Tolerable Error: '))
-N = int(input('Maximum Step: '))
 
 # Starting Newton Raphson Method
-newtonRaphson(x0,e,N)
+newtonRaphson(x0,e)
